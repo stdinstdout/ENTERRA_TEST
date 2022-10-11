738f59ff73bcdeffc6efc579439d44b4bc17df2b
Kostya Pechenenko <kpechenenko@gmail.com>
2022-10-11 18:29:04 +0700

    refactor: do not distribute the secret api key

--- weather_app/config.py
+++ weather_app/config.py
@@ -2,7 +2,6 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    # TODO: из переменных окружения
     API_KEY: str = "80a4796ed267b015ea14d7cecf5dde57"
     STORE_CACHE_TIME: int = 3600
     LOCAL_REDIS_URL: str = 'redis://redis'
